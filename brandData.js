4acc2f9f92ffe68d13110415b060590b707b6c8f
Bethan Cropper <Bethan.Cropper03@thehutgroup.com>
2022-11-16 10:40:20 +0000

    added images and brandData list

---
+++ brandData.js
@@ -0,0 +1,766 @@
+const img_dir = PUT PATH TO IMAGES HERE;
+
+export const brands = [
+  {
+    siteName: "Allsole",
+    siteID: 53,
+    img: require(img_dir + "allsoleLogo.jpeg"),
+  },
+  {
+    siteName: "The Hut",
+    siteID: 79,
+    img: require(img_dir + "theHutLogo.png"),
+  },
+  {
+    siteName: "MyProtein",
+    siteID: 83,
+    img: require(img_dir + "myProteinLogo.png"),
+  },
+  {
+    siteName: "MyVitamins",
+    siteID: 86,
+    img: require(img_dir + "myVitaminsLogo.png"),
+  },
+  {
+    siteName: "Pro Bike Kit",
+    siteID: 90,
+    img: require(img_dir + "proBikeKitLogo.png"),
+  },
+  {
+    siteName: "IWOOT",
+    siteID: 92,
+    img: require(img_dir + "iwootLogo.png"),
+  },
+  {
+    siteName: "exante",
+    siteID: 93,
+    img: require(img_dir + "exanteLogo.png"),
+  },
+  {
+    siteName: "Coggles",
+    siteID: 94,
+    img: require(img_dir + "cogglesLogo.png"),
+  },
+  {
+    siteName: "LOOKFANTASTIC",
+    siteID: 95,
+    img: require(img_dir + "lookFantasticLogo.png"),
+  },
+  {
+    siteName: "Zavvi",
+    siteID: 96,
+    img: require(img_dir + "zavviLogo.png"),
+  },
+  {
+    siteName: "Mankind",
+    siteID: 99,
+    img: require(img_dir + "mankindLogo.png"),
+  },
+  {
+    siteName: "Mass",
+    siteID: 101,
+    img: require(img_dir + "massLogo.jpeg"),
+  },
+  {
+    siteName: "Powerman",
+    siteID: 104,
+    img: require(img_dir + "powermanLogo.png"),
+  },
+  {
+    siteName: "HQHair International",
+    siteID: 105,
+    img: require(img_dir + "hqhairLogo.png"),
+  },
+  {
+    siteName: "Beauty Expert",
+    siteID: 106,
+    img: require(img_dir + "beautyExpertLogo.jpeg"),
+  },
+  {
+    siteName: "MyBag",
+    siteID: 107,
+    img: require(img_dir + "mybagLogo.png"),
+  },
+  {
+    siteName: "Pop In A Box",
+    siteID: 111,
+    img: require(img_dir + "popInABoxLogo.png"),
+  },
+  {
+    siteName: "My Geek Box",
+    siteID: 112,
+    img: require(img_dir + "myGeekBoxLogo.png"),
+  },
+  {
+    siteName: "Mio Skincare",
+    siteID: 114,
+    img: require(img_dir + "mioSkincareLogo.png"),
+  },
+  {
+    siteName: "Honda",
+    siteID: 119,
+    img: require(img_dir + "hondaLogo.png"),
+  },
+  {
+    siteName: "Grow Gorgeous ",
+    siteID: 120,
+    img: require(img_dir + "growGorgeousLogo.png"),
+  },
+  {
+    siteName: "SkinStore",
+    siteID: 121,
+    img: require(img_dir + "skinstoreLogo.jpeg"),
+  },
+  {
+    siteName: "IdealFit",
+    siteID: 125,
+    img: require(img_dir + "idealfitLogo.jpeg"),
+  },
+  {
+    siteName: "SkinCareRX",
+    siteID: 128,
+    img: require(img_dir + "skincarerxLogo.jpeg"),
+  },
+  {
+    siteName: "GLOSSYBOX",
+    siteID: 129,
+    img: require(img_dir + "glossyBoxLogo.png"),
+  },
+  { siteName: "RY", siteID: 132, img: require(img_dir + "ryLogo.jpeg") },
+  {
+    siteName: "FacialCo",
+    siteID: 133,
+    img: require(img_dir + "facialcoLogo.png"),
+  },
+  {
+    siteName: "Illamasqua",
+    siteID: 134,
+    img: require(img_dir + "illamasquaLogo.png"),
+  },
+  {
+    siteName: "ESPA",
+    siteID: 135,
+    img: require(img_dir + "espaLogo.jpeg"),
+  },
+  {
+    siteName: "IdealEssentials",
+    siteID: 137,
+    img: require(img_dir + "idealEssentialsLogo.png"),
+  },
+  {
+    siteName: "Eyeko",
+    siteID: 138,
+    img: require(img_dir + "eyekoLogo.png"),
+  },
+  {
+    siteName: "Purina Breeders",
+    siteID: 141,
+    img: require(img_dir + "purinaLogo.png"),
+  },
+  {
+    siteName: "Purina Owners",
+    siteID: 143,
+    img: require(img_dir + "purinaLogo.png"),
+  },
+  {
+    siteName: "Ameliorate",
+    siteID: 145,
+    img: require(img_dir + "ameliorateLogo.png"),
+  },
+  {
+    siteName: "Christophe Robin",
+    siteID: 146,
+    img: require(img_dir + "christopheRobinLogo.png"),
+  },
+  {
+    siteName: "Gillette",
+    siteID: 147,
+    img: require(img_dir + "gilletteLogo.jpeg"),
+  },
+  {
+    siteName: "Myvegan",
+    siteID: 149,
+    img: require(img_dir + "myVeganLogo.png"),
+  },
+  {
+    siteName: "Garden Of Life",
+    siteID: 153,
+    img: require(img_dir + "gardenOfLifeLogo.png"),
+  },
+  {
+    siteName: "Purina Breeders Beta",
+    siteID: 154,
+    img: require(img_dir + "purinaLogo.png"),
+  },
+  {
+    siteName: "No7",
+    siteID: 155,
+    img: require(img_dir + "no7Logo.png"),
+  },
+  {
+    siteName: "Oral B",
+    siteID: 159,
+    img: require(img_dir + "oralBLogo.png"),
+  },
+  {
+    siteName: "Optifast",
+    siteID: 161,
+    img: require(img_dir + "optifastLogo.webp"),
+  },
+  { siteName: "MP", siteID: 162, img: require(img_dir + "mpLogo.png") },
+  {
+    siteName: "Mercedes Benz Formula E",
+    siteID: 163,
+    img: require(img_dir + "mercedesBenzFormulaELogo.jpeg"),
+  },
+  {
+    siteName: "Burt's Bees",
+    siteID: 164,
+    img: require(img_dir + "burtsBeesLogo.png"),
+  },
+  {
+    siteName: "Elemis",
+    siteID: 165,
+    img: require(img_dir + "elemisLogo.png"),
+  },
+  {
+    siteName: "Purina Direct",
+    siteID: 166,
+    img: require(img_dir + "purinaLogo.png"),
+  },
+  {
+    siteName: "Braun",
+    siteID: 167,
+    img: require(img_dir + "braunLogo.svg"),
+  },
+  {
+    siteName: "Minami",
+    siteID: 168,
+    img: require(img_dir + "minamiLogo.png"),
+  },
+  {
+    siteName: "Klean Athlete",
+    siteID: 169,
+    img: require(img_dir + "kleanAthleteLogo.webp"),
+  },
+  {
+    siteName: "Nuxe",
+    siteID: 170,
+    img: require(img_dir + "nuxeLogo.png"),
+  },
+  {
+    siteName: "VeryNeko",
+    siteID: 173,
+    img: require(img_dir + "veryNekoLogo.png"),
+  },
+  {
+    siteName: "Perricone MD",
+    siteID: 180,
+    img: require(img_dir + "perriconeMDLogo.jpeg"),
+  },
+  {
+    siteName: "Homebase",
+    siteID: 182,
+    img: require(img_dir + "homebaseLogo.gif"),
+  },
+  {
+    siteName: "Every Health",
+    siteID: 184,
+    img: require(img_dir + "everyHealthLogo.png"),
+  },
+  {
+    siteName: "Revolution Beauty",
+    siteID: 185,
+    img: require(img_dir + "revolutionLogo.png"),
+  },
+  {
+    siteName: "Omorovicza",
+    siteID: 189,
+    img: require(img_dir + "omorovicsaLogo.png"),
+  },
+  {
+    siteName: "Your Coca-Cola",
+    siteID: 190,
+    img: require(img_dir + "yourCocaColaLogo.png"),
+  },
+  {
+    siteName: "SpoonfulOne",
+    siteID: 191,
+    img: require(img_dir + "spoonfulOneLogo.webp"),
+  },
+  {
+    siteName: "Antipodes",
+    siteID: 194,
+    img: require(img_dir + "antipodesLogo.png"),
+  },
+  {
+    siteName: "Acnecide",
+    siteID: 195,
+    img: require(img_dir + "acnecideLogo.png"),
+  },
+  {
+    siteName: "Vita Coco",
+    siteID: 197,
+    img: require(img_dir + "vitaCocoLogo.png"),
+  },
+  {
+    siteName: "Vital Proteins",
+    siteID: 198,
+    img: require(img_dir + "vitalProteinsLogo.png"),
+  },
+  {
+    siteName: "Purina Pro Plan",
+    siteID: 200,
+    img: require(img_dir + "purinaLogo.png"),
+  },
+  {
+    siteName: "Purina Vet",
+    siteID: 201,
+    img: require(img_dir + "purinaLogo.png"),
+  },
+  {
+    siteName: "Purina Vet IVC",
+    siteID: 207,
+    img: require(img_dir + "purinaLogo.png"),
+  },
+  {
+    siteName: "Dermstore",
+    siteID: 208,
+    img: require(img_dir + "dermstoreLogo.png"),
+  },
+  {
+    siteName: "Jack Wills",
+    siteID: 209,
+    img: require(img_dir + "jackWillsLogo.png"),
+  },
+  {
+    siteName: "Lancer Skincare",
+    siteID: 210,
+    img: require(img_dir + "lancerSkincareLogo.jpeg"),
+  },
+  {
+    siteName: "Creed",
+    siteID: 216,
+    img: require(img_dir + "creedLogo.png"),
+  },
+  {
+    siteName: "Bathstore",
+    siteID: 217,
+    img: require(img_dir + "bathstoreLogo.png"),
+  },
+  {
+    siteName: "Erborian",
+    siteID: 218,
+    img: require(img_dir + "erborianLogo.webp"),
+  },
+  {
+    siteName: "Team Command",
+    siteID: 222,
+    img: require(img_dir + "teamCommandLogo.png"),
+  },
+  {
+    siteName: "Morvelo",
+    siteID: 223,
+    img: require(img_dir + "morveloLogo.png"),
+  },
+  {
+    siteName: "Orlebar Brown",
+    siteID: 224,
+    img: require(img_dir + "orlebarBrownLogo.png"),
+  },
+  {
+    siteName: "Endura Sport",
+    siteID: 225,
+    img: require(img_dir + "enduraSportLogo.png"),
+  },
+  {
+    siteName: "Sukin Naturals",
+    siteID: 228,
+    img: require(img_dir + "sukinNaturalsLogo.png"),
+  },
+  {
+    siteName: "Arrow Films",
+    siteID: 230,
+    img: require(img_dir + "arrowFilmsLogo.png"),
+  },
+  {
+    siteName: "Clink Spirit",
+    siteID: 231,
+    img: require(img_dir + "clinkSpiritLogo.png"),
+  },
+  {
+    siteName: "Kellica",
+    siteID: 233,
+    img: require(img_dir + "kellicaLogo.png"),
+  },
+  {
+    siteName: "SpectriSKIN",
+    siteID: 234,
+    img: require(img_dir + "spectriSkinLogo.png"),
+  },
+  {
+    siteName: "Zyn",
+    siteID: 236,
+    img: require(img_dir + "zynLogo.png"),
+  },
+  {
+    siteName: "Lor Espresso",
+    siteID: 238,
+    img: require(img_dir + "lorEspressoLogo.jpeg"),
+  },
+  {
+    siteName: "Toblerone",
+    siteID: 239,
+    img: require(img_dir + "tobleroneLogo.png"),
+  },
+  {
+    siteName: "Croquons La Vie",
+    siteID: 240,
+    img: require(img_dir + "croquonsLaVieLogo.jpeg"),
+  },
+  {
+    siteName: "Speedo",
+    siteID: 241,
+    img: require(img_dir + "speedoLogo.png"),
+  },
+  {
+    siteName: "Mama Mio",
+    siteID: 244,
+    img: require(img_dir + "mamaMioLogo.png"),
+  },
+  {
+    siteName: "Melvita",
+    siteID: 246,
+    img: require(img_dir + "melvitaLogo.png"),
+  },
+  {
+    siteName: "Berghaus",
+    siteID: 248,
+    img: require(img_dir + "berghausLogo.png"),
+  },
+  {
+    siteName: "Comvita",
+    siteID: 249,
+    img: require(img_dir + "comvitaLogo.png"),
+  },
+  {
+    siteName: "Andalou Naturals",
+    siteID: 251,
+    img: require(img_dir + "andalouNaturalsLogo.png"),
+  },
+  {
+    siteName: "Clogau Outlet",
+    siteID: 253,
+    img: require(img_dir + "clogauOutletLogo.png"),
+  },
+  {
+    siteName: "SFI Health",
+    siteID: 254,
+    img: require(img_dir + "sfiHealthLogo.jpeg"),
+  },
+  {
+    siteName: "Kickers",
+    siteID: 255,
+    img: require(img_dir + "kickersLogo.gif"),
+  },
+  {
+    siteName: "THG Training",
+    siteID: 256,
+    img: require(img_dir + "thgLogo.png"),
+  },
+  {
+    siteName: "Cult Beauty",
+    siteID: 257,
+    img: require(img_dir + "cultBeautyLogo.png"),
+  },
+  {
+    siteName: "HandmadeSoapCompany",
+    siteID: 259,
+    img: require(img_dir + "handmadeSoapCompanyLogo.webp"),
+  },
+  {
+    siteName: "Ellesse",
+    siteID: 261,
+    img: require(img_dir + "ellesseLogo.png"),
+  },
+  {
+    siteName: "11 Degrees",
+    siteID: 262,
+    img: require(img_dir + "11DegreesLogo.png"),
+  },
+  {
+    siteName: "Natures Truth",
+    siteID: 263,
+    img: require(img_dir + "naturesTruthLogo.png"),
+  },
+  {
+    siteName: "Canterbury",
+    siteID: 265,
+    img: require(img_dir + "canterburyLogo.png"),
+  },
+  {
+    siteName: "Pureis CBD",
+    siteID: 266,
+    img: require(img_dir + "pureisCBDLogo.png"),
+  },
+  {
+    siteName: "Unishop",
+    siteID: 267,
+    img: require(img_dir + "unishopLogo.webp"),
+  },
+  {
+    siteName: "Wella",
+    siteID: 268,
+    img: require(img_dir + "wellaLogo.jpeg"),
+  },
+  {
+    siteName: "MyPro",
+    siteID: 271,
+    img: require(img_dir + "myProLogo.png"),
+  },
+  {
+    siteName: "Revlon",
+    siteID: 272,
+    img: require(img_dir + "revlonLogo.png"),
+  },
+  {
+    siteName: "The Unexpekted",
+    siteID: 273,
+    img: require(img_dir + "theUnexpektedLogo.png"),
+  },
+  {
+    siteName: "Amika",
+    siteID: 276,
+    img: require(img_dir + "amikaLogo.png"),
+  },
+  {
+    siteName: "BeNu",
+    siteID: 277,
+    img: require(img_dir + "benuLogo.jpeg"),
+  },
+  {
+    siteName: "Dr Brandt",
+    siteID: 278,
+    img: require(img_dir + "drBrandtLogo.png"),
+  },
+  {
+    siteName: "HPE Activewear",
+    siteID: 279,
+    img: require(img_dir + "hpeActivewearLogo.png"),
+  },
+  {
+    siteName: "Athletia Beauty",
+    siteID: 280,
+    img: require(img_dir + "athletiaBeautyLogo.png"),
+  },
+  {
+    siteName: "David Gandy Wellwear",
+    siteID: 283,
+    img: require(img_dir + "davidGandyWellwearLogo.png"),
+  },
+  {
+    siteName: "TRR Nutrition",
+    siteID: 284,
+    img: require(img_dir + "trrNutritionLogo.png"),
+  },
+  {
+    siteName: "LQ Collagen",
+    siteID: 285,
+    img: require(img_dir + "lqCollagenLogo.png"),
+  },
+  {
+    siteName: "Filorga",
+    siteID: 286,
+    img: require(img_dir + "filorgaLogo.jpeg"),
+  },
+  {
+    siteName: "Frank Body",
+    siteID: 289,
+    img: require(img_dir + "frankBodyLogo.jpeg"),
+  },
+  {
+    siteName: "By Terry",
+    siteID: 290,
+    img: require(img_dir + "byTerryLogo.png"),
+  },
+  {
+    siteName: "Bonraw Foods",
+    siteID: 292,
+    img: require(img_dir + "bonrawFoodsLogo.png"),
+  },
+  {
+    siteName: "Precision Biotics",
+    siteID: 293,
+    img: require(img_dir + "precisionBioticsLogo.webp"),
+  },
+  {
+    siteName: "ORA Skincare",
+    siteID: 295,
+    img: require(img_dir + "oraSkincareLogo.png"),
+  },
+  {
+    siteName: "Mighty",
+    siteID: 296,
+    img: require(img_dir + "mightyLogo.png"),
+  },
+  {
+    siteName: "PN Selfcare",
+    siteID: 297,
+    img: require(img_dir + "pnSelfcareLogo.png"),
+  },
+  {
+    siteName: "Baby & Me",
+    siteID: 299,
+    img: require(img_dir + "babyAndMeLogo.png"),
+  },
+  {
+    siteName: "Artdeco Cosmetics",
+    siteID: 303,
+    img: require(img_dir + "artdecoCosmeticsLogo.png"),
+  },
+  {
+    siteName: "Puritan's Pride",
+    siteID: 304,
+    img: require(img_dir + "puritansPrideLogo.webp"),
+  },
+  {
+    siteName: "Plenaire",
+    siteID: 305,
+    img: require(img_dir + "plenaireLogo.png"),
+  },
+  {
+    siteName: "Gearbox",
+    siteID: 306,
+    img: require(img_dir + "gearboxLogo.png"),
+  },
+  {
+    siteName: "Jersey Hemp",
+    siteID: 307,
+    img: require(img_dir + "jerseyHempLogo.png"),
+  },
+  {
+    siteName: "Boostball",
+    siteID: 309,
+    img: require(img_dir + "boostballLogo.png"),
+  },
+  {
+    siteName: "ACO",
+    siteID: 310,
+    img: require(img_dir + "acoLogo.png"),
+  },
+  {
+    siteName: "Molton Brown",
+    siteID: 311,
+    img: require(img_dir + "moltonBrownLogo.png"),
+  },
+  {
+    siteName: "Bimuno",
+    siteID: 312,
+    img: require(img_dir + "bimunoLogo.png"),
+  },
+  {
+    siteName: "Nutrimuscle",
+    siteID: 313,
+    img: require(img_dir + "nutrimuscleLogo.webp"),
+  },
+  {
+    siteName: "Shiro",
+    siteID: 315,
+    img: require(img_dir + "shiroLogo.png"),
+  },
+  {
+    siteName: "Indie Lee",
+    siteID: 316,
+    img: require(img_dir + "indieLeeLogo.png"),
+  },
+  {
+    siteName: "Rock and Roll Beauty",
+    siteID: 317,
+    img: require(img_dir + "rockAndRollBeautyLogo.png"),
+  },
+  {
+    siteName: "Myrkl",
+    siteID: 318,
+    img: require(img_dir + "myrklLogo.png"),
+  },
+  {
+    siteName: "Bio-Kult",
+    siteID: 320,
+    img: require(img_dir + "bioKultLogo.png"),
+  },
+  {
+    siteName: "Fibre One",
+    siteID: 324,
+    img: require(img_dir + "fibreOneLogo.jpeg"),
+  },
+  {
+    siteName: "Begg x Co",
+    siteID: 326,
+    img: require(img_dir + "beggxcoLogo.jpeg"),
+  },
+  {
+    siteName: "Millican",
+    siteID: 327,
+    img: require(img_dir + "millicanLogo.webp"),
+  },
+  {
+    siteName: "Inika Superfoods",
+    siteID: 328,
+    img: require(img_dir + "inikaSuperfoodsLogo.png"),
+  },
+  {
+    siteName: "Prolong",
+    siteID: 329,
+    img: require(img_dir + "prolongLogo.jpeg"),
+  },
+  {
+    siteName: "Drybar",
+    siteID: 330,
+    img: require(img_dir + "drybarLogo.jpeg"),
+  },
+  {
+    siteName: "BH Cosmetics",
+    siteID: 331,
+    img: require(img_dir + "bhCosmeticsLogo.jpeg"),
+  },
+  {
+    siteName: "Korres",
+    siteID: 334,
+    img: require(img_dir + "korresLogo.png"),
+  },
+  {
+    siteName: "Oneskee",
+    siteID: 337,
+    img: require(img_dir + "oneskeeLogo.jpeg"),
+  },
+  {
+    siteName: "Born Primitive",
+    siteID: 338,
+    img: require(img_dir + "bornPrimitiveLogo.jpeg"),
+  },
+  {
+    siteName: "Sknhead",
+    siteID: 339,
+    img: require(img_dir + "sknheadLogo.jpeg"),
+  },
+  {
+    siteName: "Celebrity Slim",
+    siteID: 341,
+    img: require(img_dir + "celebritySlimLogo.png"),
+  },
+  {
+    siteName: "Anastasia BH",
+    siteID: 344,
+    img: require(img_dir + "anastasiaBHLogo.jpeg"),
+  },
+  {
+    siteName: "Movementum",
+    siteID: 347,
+    img: require(img_dir + "movementumLogo.png"),
+  },
+  {
+    siteName: "About Time We Met",
+    siteID: 351,
+    img: require(img_dir + "aboutTimeWeMetLogo.jpeg"),
+  },
+];
